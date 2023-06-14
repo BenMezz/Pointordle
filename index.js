e5a414ad77e402af050a79a6a81832e76ab2e3bc
BenMezz <118441306+BenMezz@users.noreply.github.com>
2023-06-13 20:03:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ let animating = false;
 const beg_date = new Date('05/25/2023');
 const current_date = new Date();
 
-const Difference_In_Days = (current_date.getTime() - beg_date.getTime()) / (1000 * 3600 * 24);
+var Difference_In_Days = (current_date.getTime() - beg_date.getTime()) / (1000 * 3600 * 24);
 
 let localstorage_num = 0;
 while (true) {
-  if (!localStorage.hasOwnProperty(localstorage_num + 1)) break;
-  localstorage_num++;
+	if (!localStorage.hasOwnProperty(localstorage_num + 1)) break;
+	localstorage_num++;
 }
 
 const container = document.querySelector('.container');
@@ -56,7 +56,7 @@ if (answer.length <= 5) {
 }
 
 if (localStorage.getItem(`${localstorage_num}_date`) == Math.floor(Difference_In_Days)) {
-  answer_word(localStorage.getItem(`${localstorage_num}_words`))
+	answer_word(localStorage.getItem(`${localstorage_num}_words`))
 }
 
 if (localStorage.hasOwnProperty(`${localstorage_num}_words`)) {
@@ -77,32 +77,32 @@ let current_guess = 0;
 let score = 0;
 
 function answer_word(word_str) {
-  let wordsfo = [];
-  for (let i = 0; i < word_str.length / answer.length; i++) {
-    wordsfo.push(word_str.slice((i) * answer.length, (i + 1) * answer.length));
-  }
-  let a = 0;
-  let b = 0;
-  for (let i = 0; i < word_str.length; i++) {
-    if (b == answer.length) {
-      b = 0;
-      a++;
-    };
-    if (wordsfo[a].charAt(b) == answer.charAt(b)) {
-      document.querySelector(`.container > div:nth-child(${a + 1}) > div:nth-child(${b + 1})`).style.backgroundColor = 'var(--green)';
-      document.querySelector(`.container > div:nth-child(${a + 1}) > div:nth-child(${b + 1})`).style.border = '2px solid var(--green)';
-      document.querySelector(`.container > div:nth-child(${a + 1}) > div:nth-child(${b + 1})`).innerHTML = wordsfo[a].charAt(b);
-    } else if (answer.includes(wordsfo[a].charAt(b))) {
-      document.querySelector(`.container > div:nth-child(${a + 1}) > div:nth-child(${b + 1})`).style.backgroundColor = 'var(--yellow)';
-      document.querySelector(`.container > div:nth-child(${a + 1}) > div:nth-child(${b + 1})`).style.border = '2px solid var(--yellow)';
-      document.querySelector(`.container > div:nth-child(${a + 1}) > div:nth-child(${b + 1})`).innerHTML = wordsfo[a].charAt(b);
-    } else {
-      document.querySelector(`.container > div:nth-child(${a + 1}) > div:nth-child(${b + 1})`).style.backgroundColor = 'var(--gray)';
-      document.querySelector(`.container > div:nth-child(${a + 1}) > div:nth-child(${b + 1})`).style.border = '2px solid var(--gray)';
-      document.querySelector(`.container > div:nth-child(${a + 1}) > div:nth-child(${b + 1})`).innerHTML = wordsfo[a].charAt(b);
-    }
-    b++;
-  }
+	let wordsfo = [];
+	for (let i = 0; i < word_str.length / answer.length; i++) {
+		wordsfo.push(word_str.slice((i) * answer.length, (i + 1) * answer.length));
+	}
+	let a = 0;
+	let b = 0;
+	for (let i = 0; i < word_str.length; i++) {
+		if (b == answer.length) {
+			b = 0;
+			a++;
+		};
+		if (wordsfo[a].charAt(b) == answer.charAt(b)) {
+			document.querySelector(`.container > div:nth-child(${a + 1}) > div:nth-child(${b + 1})`).style.backgroundColor = 'var(--green)';
+			document.querySelector(`.container > div:nth-child(${a + 1}) > div:nth-child(${b + 1})`).style.border = '2px solid var(--green)';
+			document.querySelector(`.container > div:nth-child(${a + 1}) > div:nth-child(${b + 1})`).innerHTML = wordsfo[a].charAt(b);
+		} else if (answer.includes(wordsfo[a].charAt(b))) {
+			document.querySelector(`.container > div:nth-child(${a + 1}) > div:nth-child(${b + 1})`).style.backgroundColor = 'var(--yellow)';
+			document.querySelector(`.container > div:nth-child(${a + 1}) > div:nth-child(${b + 1})`).style.border = '2px solid var(--yellow)';
+			document.querySelector(`.container > div:nth-child(${a + 1}) > div:nth-child(${b + 1})`).innerHTML = wordsfo[a].charAt(b);
+		} else {
+			document.querySelector(`.container > div:nth-child(${a + 1}) > div:nth-child(${b + 1})`).style.backgroundColor = 'var(--gray)';
+			document.querySelector(`.container > div:nth-child(${a + 1}) > div:nth-child(${b + 1})`).style.border = '2px solid var(--gray)';
+			document.querySelector(`.container > div:nth-child(${a + 1}) > div:nth-child(${b + 1})`).innerHTML = wordsfo[a].charAt(b);
+		}
+		b++;
+	}
 }
 
 localStorage.setItem(`${localstorage_num + 1}_date`, Math.floor(Difference_In_Days));
@@ -126,18 +126,25 @@ function press(key) {
 			current_active_tile.removeAttribute('id');
 			current_active_tile.nextElementSibling.innerHTML = key;
 		} else if (current_active_tile.nextElementSibling === null && current_active_tile != null) {
-      if (key != 'Enter') return;
+			if (key != 'Enter') return;
 			let word = '';
 			for (let i = 0; i < current_active_tile.parentElement.childElementCount; i++) {
 				word += document.querySelector(`.container > .${current_active_tile.parentElement.className} > div:nth-child(${i + 1})`).innerHTML.toLowerCase();
 			}
 			if (!words.includes(word)) return;
-      animating = true;
-      if (localStorage.hasOwnProperty(`${localstorage_num + 1}_words`)) {
-        localStorage.setItem(`${localstorage_num + 1}_words`, localStorage.getItem(`${localstorage_num + 1}_words`) + word);
-      } else {
-        localStorage.setItem(`${localstorage_num + 1}_words`, word);
-      }
+			// add points to localstorrage if won
+			if (word == answer) {
+				const empty_cells = document.querySelectorAll('.container > * > *:not(.animate)');
+				const num_of_rows = container.childElementCount;
+				const points = Math.round(((score + empty_cells.length) / (answer.length * num_of_rows)) * lp + ((num_of_rows - (current_guess - 1)) / num_of_rows) * tp);
+				localStorage.setItem(localstorage_num + 1, points);
+			}
+			animating = true;
+			if (localStorage.hasOwnProperty(`${localstorage_num + 1}_words`)) {
+				localStorage.setItem(`${localstorage_num + 1}_words`, localStorage.getItem(`${localstorage_num + 1}_words`) + word);
+			} else {
+				localStorage.setItem(`${localstorage_num + 1}_words`, word);
+			}
 			current_active_tile.removeAttribute('id');
 			let a = current_active_tile.parentElement.nextElementSibling || '';
 			if (a != '') {
@@ -161,7 +168,12 @@ function press(key) {
 			}
 			document.querySelector(`.container > .${current_active_tile.parentElement.className} > div:last-child`).addEventListener('animationend', () => {
 				if (word == answer) {
-					winGame();
+					for (let i = 0; i < answer.length; i++) {
+						document.querySelector(`.container > .${current_active_tile.parentElement.className} > div:nth-child(${i + 1})`).classList.add('animate3');
+					}
+					document.querySelector(`.container > .${current_active_tile.parentElement.className} > div:last-child`).addEventListener('animationend', () => {
+						winGame();
+					});
 				}
 			});
 			for (let i = 0; i < current_active_tile.parentElement.childElementCount; i++) {
@@ -223,79 +235,81 @@ function eventfunc(e) {
 
 // Won Game
 function winGame() {
-  let points;
-  let last_points;
-  let second_to_last_points;
-  let third_to_last_points;
-  if (localStorage.getItem(`${localstorage_num}_date`) != Math.floor(Difference_In_Days)) {
-    const empty_cells = document.querySelectorAll('.container > * > *:not(.animate)');
-    const num_of_rows = container.childElementCount;
-    points = Math.round(((score + empty_cells.length) / (answer.length * num_of_rows)) * lp + ((num_of_rows - (current_guess - 1)) / num_of_rows) * tp);
+	let points;
+	let last_points;
+	let second_to_last_points;
+	let third_to_last_points;
+	if (localStorage.getItem(`${localstorage_num}_date`) != Math.floor(Difference_In_Days)) {
+		const empty_cells = document.querySelectorAll('.container > * > *:not(.animate)');
+		const num_of_rows = container.childElementCount;
+		points = Math.round(((score + empty_cells.length) / (answer.length * num_of_rows)) * lp + ((num_of_rows - (current_guess - 1)) / num_of_rows) * tp);
 
-    // Localstorage Setting
-    localStorage.setItem(localstorage_num + 1, points);
+		// Localstorage Setting
+		last_points = localStorage.getItem(localstorage_num) || '0';
+		second_to_last_points = localStorage.getItem(localstorage_num - 1) || '0';
+		third_to_last_points = localStorage.getItem(localstorage_num - 2) || '0';
+		document.querySelector('.points-section > div:nth-child(2) > p').innerHTML = `${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num}_date`)))).getMonth()}/${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num}_date`)))).getDate()}`;
+		if (document.querySelector('.points-section > div:nth-child(2) > p').innerHTML == 'NaN/NaN') {
+			document.querySelector('.points-section > div:nth-child(2) > p').innerHTML = '0/0'
+		}
+		document.querySelector('.points-section > div:nth-child(3) > p').innerHTML = `${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num - 1}_date`)))).getMonth()}/${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num - 1}_date`)))).getDate()}`;
+		if (document.querySelector('.points-section > div:nth-child(3) > p').innerHTML == 'NaN/NaN') {
+			document.querySelector('.points-section > div:nth-child(3) > p').innerHTML = '0/0'
+		}
+		document.querySelector('.points-section > div:nth-child(4) > p').innerHTML = `${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num - 2}_date`)))).getMonth()}/${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num - 2}_date`)))).getDate()}`;
+		if (document.querySelector('.points-section > div:nth-child(4) > p').innerHTML == 'NaN/NaN') {
+			document.querySelector('.points-section > div:nth-child(4) > p').innerHTML = '0/0'
+		}
+	} else {
+		points = localStorage.getItem(localstorage_num)
+		last_points = localStorage.getItem(localstorage_num - 1) || '0';
+		second_to_last_points = localStorage.getItem(localstorage_num - 2) || '0';
+		third_to_last_points = localStorage.getItem(localstorage_num - 3) || '0';
+		document.querySelector('.points-section > div:nth-child(2) > p').innerHTML = `${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num - 1}_date`)))).getMonth()}/${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num - 1}_date`)))).getDate()}`;
+		if (document.querySelector('.points-section > div:nth-child(2) > p').innerHTML == 'NaN/NaN') {
+			document.querySelector('.points-section > div:nth-child(2) > p').innerHTML = '0/0'
+		}
+		document.querySelector('.points-section > div:nth-child(3) > p').innerHTML = `${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num - 2}_date`)))).getMonth()}/${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num - 2}_date`)))).getDate()}`;
+		if (document.querySelector('.points-section > div:nth-child(3) > p').innerHTML == 'NaN/NaN') {
+			document.querySelector('.points-section > div:nth-child(3) > p').innerHTML = '0/0'
+		}
+		document.querySelector('.points-section > div:nth-child(4) > p').innerHTML = `${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num - 3}_date`)))).getMonth()}/${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num - 3}_date`)))).getDate()}`;
+		if (document.querySelector('.points-section > div:nth-child(4) > p').innerHTML == 'NaN/NaN') {
+			document.querySelector('.points-section > div:nth-child(4) > p').innerHTML = '0/0'
+		}
+	}
 
-    last_points = localStorage.getItem(localstorage_num) || '0';
-	  second_to_last_points = localStorage.getItem(localstorage_num - 1) || '0';
-	  third_to_last_points = localStorage.getItem(localstorage_num - 2) || '0';
-    document.querySelector('.points-section > div:nth-child(2) > p').innerHTML = `${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num}_date`)))).getMonth()}/${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num}_date`)))).getDate()}`;
-    if (document.querySelector('.points-section > div:nth-child(2) > p').innerHTML == 'NaN/NaN') {
-      document.querySelector('.points-section > div:nth-child(2) > p').innerHTML = '0/0'
-    }
-    document.querySelector('.points-section > div:nth-child(3) > p').innerHTML = `${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num - 1}_date`)))).getMonth()}/${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num - 1}_date`)))).getDate()}`;
-    if (document.querySelector('.points-section > div:nth-child(3) > p').innerHTML == 'NaN/NaN') {
-      document.querySelector('.points-section > div:nth-child(3) > p').innerHTML = '0/0'
-    }
-    document.querySelector('.points-section > div:nth-child(4) > p').innerHTML = `${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num - 2}_date`)))).getMonth()}/${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num - 2}_date`)))).getDate()}`;
-    if (document.querySelector('.points-section > div:nth-child(4) > p').innerHTML == 'NaN/NaN') {
-      document.querySelector('.points-section > div:nth-child(4) > p').innerHTML = '0/0'
-    }
-  } else {
-    points = localStorage.getItem(localstorage_num)
-    last_points = localStorage.getItem(localstorage_num - 1) || '0';
-	  second_to_last_points = localStorage.getItem(localstorage_num - 2) || '0';
-	  third_to_last_points = localStorage.getItem(localstorage_num - 3) || '0';
-    document.querySelector('.points-section > div:nth-child(2) > p').innerHTML = `${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num - 1}_date`)))).getMonth()}/${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num - 1}_date`)))).getDate()}`;
-    if (document.querySelector('.points-section > div:nth-child(2) > p').innerHTML == 'NaN/NaN') {
-      document.querySelector('.points-section > div:nth-child(2) > p').innerHTML = '0/0'
-    }
-    document.querySelector('.points-section > div:nth-child(3) > p').innerHTML = `${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num - 2}_date`)))).getMonth()}/${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num - 2}_date`)))).getDate()}`;
-    if (document.querySelector('.points-section > div:nth-child(3) > p').innerHTML == 'NaN/NaN') {
-      document.querySelector('.points-section > div:nth-child(3) > p').innerHTML = '0/0'
-    }
-    document.querySelector('.points-section > div:nth-child(4) > p').innerHTML = `${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num - 3}_date`)))).getMonth()}/${new Date(new Date().setDate(beg_date.getDate() + parseInt(localStorage.getItem(`${localstorage_num - 3}_date`)))).getDate()}`;
-    if (document.querySelector('.points-section > div:nth-child(4) > p').innerHTML == 'NaN/NaN') {
-      document.querySelector('.points-section > div:nth-child(4) > p').innerHTML = '0/0'
-    }
-  }
-	
-  document.removeEventListener('keydown', eventfunc);
+	document.removeEventListener('keydown', eventfunc);
 	modal.showModal();
 	modal.style.transform = 'translateY(50px)';
 	modal.style.opacity = '1';
 	modal.style.translate = '0px -50px';
 	document.querySelector('.points').innerHTML = points;
 
-	document.querySelector('.points-section > div:nth-child(2) > div > p').innerHTML = last_points;
-	document.querySelector('.points-section > div:nth-child(2) > div').style.width = `${parseInt(last_points) / 10}%`;
-  if (270 * parseInt(last_points) / 10 < 16) {
-    document.querySelector('.points-section > div:nth-child(2) > div > p').style.right = '0';
-    document.querySelector('.points-section > div:nth-child(2) > div > p').style.transform = 'translateX(-50%)';
-  }
+	const bar1 = document.querySelector('.points-section > div:nth-child(2) > div');
+	const bar2 = document.querySelector('.points-section > div:nth-child(3) > div');
+	const bar3 = document.querySelector('.points-section > div:nth-child(4) > div');
 
-	document.querySelector('.points-section > div:nth-child(3) > div > p').innerHTML = second_to_last_points;
-	document.querySelector('.points-section > div:nth-child(3) > div').style.width = `${parseInt(second_to_last_points) / 10}%`;
-  if (270 * parseInt(second_to_last_points) / 10 < 16) {
-    document.querySelector('.points-section > div:nth-child(3) > div > p').style.right = '0';
-    document.querySelector('.points-section > div:nth-child(3) > div > p').style.transform = 'translateX(-50%)';
-  }
+	bar1.firstChild.innerHTML = last_points;
+	bar1.style.width = `${parseInt(last_points) / 10}%`;
+	if (270 * parseInt(last_points) / 10 < 16) {
+		bar1.firstChild.style.right = '0';
+		bar1.firstChild.style.transform = 'translateX(-50%)';
+	}
 
-	document.querySelector('.points-section > div:nth-child(4) > div > p').innerHTML = third_to_last_points;
-	document.querySelector('.points-section > div:nth-child(4) > div').style.width = `${parseInt(third_to_last_points) / 10}%`;
-  if (270 * parseInt(third_to_last_points) / 10 < 16) {
-    document.querySelector('.points-section > div:nth-child(4) > div > p').style.right = '0';
-    document.querySelector('.points-section > div:nth-child(4) > div > p').style.transform = 'translateX(-50%)';
-  }
+	bar2.firstChild.innerHTML = second_to_last_points;
+	bar2.style.width = `${parseInt(second_to_last_points) / 10}%`;
+	if (270 * parseInt(second_to_last_points) / 10 < 16) {
+		bar2.firstChild.style.right = '0';
+		bar2.firstChild.style.transform = 'translateX(-50%)';
+	}
+
+	bar3.firstChild.innerHTML = third_to_last_points;
+	bar3.style.width = `${parseInt(third_to_last_points) / 10}%`;
+	if (270 * parseInt(third_to_last_points) / 10 < 16) {
+		bar3.firstChild.style.right = '0';
+		bar3.firstChild.style.transform = 'translateX(-50%)';
+	}
 }
 
 // Add the Close Button Listener
